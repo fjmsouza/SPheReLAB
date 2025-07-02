35ad25e81d1a66f8598cd7f7784a278a1929356e
Flávio Souza <97055092+fjmsouza@users.noreply.github.com>
2025-07-02 14:03:56 -0300

    first commit

--- SPheReSensor/lib/Credentials/YourCredentials.cpp
+++ SPheReSensor/lib/Credentials/YourCredentials.cpp
@@ -1,8 +1,10 @@
 
+#include "Credentials.h"
+
 const char *WIFI_SSID = ""; // Put here your Wi-Fi SSID
 const char *WIFI_PASS = ""; // Put here your Wi-Fi password
 
 // Google Sheets script ID
-String GOOGLE_SHEETS_SCRIPT_ID = "";
+const String GOOGLE_SHEETS_SCRIPT_ID = "";
 // Google Drive Script ID
-String GOOGLE_DRIVE_SCRIPT_ID = "";
+const String GOOGLE_DRIVE_SCRIPT_ID = "";
