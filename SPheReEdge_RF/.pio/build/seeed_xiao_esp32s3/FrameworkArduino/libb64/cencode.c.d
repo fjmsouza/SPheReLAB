061ab5797c8d683f5b0c01ecd3ee4aa5142b2c8a
Flavius Josephus <97055092+fjmsouza@users.noreply.github.com>
2025-07-02 14:14:32 -0300

    Delete SPheReEdge_RF/.pio/build directory

--- SPheReEdge_RF/.pio/build/seeed_xiao_esp32s3/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/seeed_xiao_esp32s3/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/phlav/.platformio/packages/framework-arduinoespressif32-3.20014.231204/cores/esp32/libb64/cencode.c \
- C:/Users/phlav/.platformio/packages/framework-arduinoespressif32-3.20014.231204/cores/esp32/libb64/cencode.h
--- SPheReEdge_RF/.pio/build/seeed_xiao_esp32s3/src/random_forest.c.d
+++
@@ -1,1 +0,0 @@
-.pio/build/seeed_xiao_esp32s3/src/random_forest.c.o: src/random_forest.c
